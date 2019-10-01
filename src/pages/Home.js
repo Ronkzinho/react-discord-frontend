330656fb5971a7357dd36bff06bef17a87abb599
Ronkzinho Extreme <henriquefrcr@gmail.com>
2019-10-01 14:48:52 -0300

    First frontend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,16 +7,38 @@ export class Home extends Component{
         userID
         color
         bot
+        users
+        componentDidMount(){
+          var sel = document.getElementById('select');
+          api.get("/users").then(async response => {
+            var arr1 = response.data.users
+            var arr2 = response.data.nick 
+            var arr = arr1.map(function (x, i){
+              return { user: x, nick: arr2[i]}
+            })
+            arr.map(c => {
+              var opt = document.createElement('option');
+              opt.appendChild( document.createTextNode(c.user + " - " + c.nick) );
+              opt.value = c.user; 
+              sel.appendChild(opt); 
+            })
+          })
 
+          
+        }
         render(){  
           return (
             <div className="App" >
-              <input placeholder="seu id" onInput={(e) => this.userID = e.target.value}></input>
+              Usuário: 
+              <br />
+              <select id="select"></select>
+              <br />
               <button onClick={() => this.ir()}>Ir</button>
             </div>
           );
         }
         ir(){
+          this.userID = document.getElementById("select").value
             if(!this.userID || this.userID === ""){
               return alert("Você não pode trocar de pagina sem um id")
             }
